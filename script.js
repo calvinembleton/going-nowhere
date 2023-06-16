635177294f380f8efe95fd03d7fd4ae1c98a4ca8
Calvin <aussiebeta@me.com>
2023-06-16 19:55:48 +1000

    added some sound and border growth effects

--- script.js
+++ script.js
@@ -5,18 +5,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let isFirstInput = true; // Flag to track the first input
 
+  const audioElement = new Audio('static.mp3');
+  audioElement.loop = true;
+
+  const maxBorderWidth = 150; // Maximum border width
+
   textBox.addEventListener('input', function () {
     const count = textBox.value.length;
     characterCounter.textContent = count;
 
     if (count === 0) {
-      if (!isFirstInput) { // Check if it's not the first input
-        textBox.style.borderColor = '#e6d51c'; // Update text box color
-        htmlElement.style.borderColor = '#e6d51c'; // Update HTML border color
-      }
+      textBox.style.borderColor = '#89da34';
+      htmlElement.style.borderColor = '#89da34';
+      htmlElement.style.zIndex = 'auto'; // Reset z-index
     } else {
-      textBox.style.borderColor = '#89da34'; // Update text box color
-      htmlElement.style.borderColor = '#89da34'; // Update HTML border color
+      textBox.style.borderColor = '#FF3800';
+      htmlElement.style.borderColor = '#FF3800';
+      htmlElement.style.zIndex = '9999'; // Set a high z-index value
+    }
+
+    if (isFirstInput && count > 0) {
+      const borderWidth = count <= maxBorderWidth ? count : maxBorderWidth;
+      htmlElement.style.borderWidth = `${borderWidth}px`;
+      audioElement.volume = count / 100;
+      audioElement.play();
+      isFirstInput = true;
+      htmlElement.classList.add('grain-effect'); // Add class for grain effect
+    } else if (count === 0) {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+      isFirstInput = true;
+      htmlElement.style.borderWidth = '1px'; // Reset border width
+      htmlElement.classList.remove('grain-effect'); // Remove class for grain effect
     }
   });
 
@@ -25,13 +45,14 @@ document.addEventListener('DOMContentLoaded', function () {
       event.preventDefault();
       textBox.value = '';
       characterCounter.textContent = '0';
-      textBox.style.borderColor = '#e6d51c'; // Update text box color
-      htmlElement.style.borderColor = '#e6d51c'; // Update HTML border color
-      submitSound.play(); // Play the sound when Enter is pressed
+      textBox.style.borderColor = '#89da34';
+      htmlElement.style.borderColor = '#89da34';
+      audioElement.pause();
+      audioElement.currentTime = 0;
+      isFirstInput = true;
+      htmlElement.style.borderWidth = '1px'; // Reset border width
+      htmlElement.classList.remove('grain-effect'); // Remove class for grain effect
+      htmlElement.style.zIndex = 'auto'; // Reset z-index
     }
-  });
-
-  textBox.addEventListener('focus', function () {
-    isFirstInput = false; // Update the flag on the first input
   });
 });
